2dcb9de63f0c175acc85e3bcdcaa66a9bfe948c2
hamshamso <your-email@example.com>
2026-08-21 22:32:11 +0100

    Login Controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,6 +28,26 @@ const registeruser = async(req,res) => {
     }
 }
 const loginuser = async(req,res) => {
+    try{
+        const {email,password} = req.body  || {}
+        const user = await User.findOne({email})
 
+        if(user && (await bcrypt.compare(password,user.password))){
+        const token = jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        )
+        return res.status(200).json({
+            success: true,
+            msg: "Logged in successfully",
+            data: { name: user.name, email: user.email },
+            token})
+        }
+        res.status(401).json({success:false, msg:"Password or Email inccorect"})
+    }catch(err){
+        console.error(err)
+        res.status(400).json({success: false, msg: "Something went wrong" })
+    }
 }
 export default {registeruser, loginuser}
