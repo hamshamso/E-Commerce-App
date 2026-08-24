e9a217ee4e13b0beb05c184b3a90ce8bf372d21a
hamshamso <your-email@example.com>
2026-08-24 09:54:22 +0100

    Protect Createproduct with admin middelware

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {createProduct,getAllProducts,getProductById} from '../controllers/productController.js'
+import admin from '../middelwares/adminOnly.js';
+
 const Router = express.Router();
 
-Router.post('/products',createProduct)
+Router.post('/products',admin,createProduct)
 Router.get('/products/:id/',getProductById)
 Router.get('/products',getAllProducts)
 
