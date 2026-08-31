7a71288458e8f949dcb344d5b03e4f767376cf92
hamshamso <hamchamco7@gmail.com>
2026-08-31 20:54:29 +0100

    Add new stuff in cart page

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,16 +1,24 @@
 import { useCart } from "../context/ProductContext";
 import "../styles/cart.css";
 import trashIcon from "../assets/trush.png";
+import cartIcon from "../assets/cart.png"
+import { useNavigate } from "react-router-dom";
 
 export function Cart(){
   //100% engineernig (no vibecoding)
     const {cart,removeFromCart} = useCart()
+    const Navigate = useNavigate()
+
     return(
       <>
         {cart.length === 0 ? 
           <div className="empty">
             <h1>Your cart is empty</h1>
-            <h3>Buy some products and view it here</h3>
+            <h2>Buy some products and view it here</h2>
+            <div className="buy" onClick={() =>{Navigate('/')}}>
+                  <h3 className="buy-text">Shop</h3>
+                  <img src={cartIcon} alt="cart Icon"/> 
+            </div>
           </div> :
             <div className="cart">
               <h1 className="title">Your cart products</h1>
@@ -28,8 +36,13 @@ export function Cart(){
                     </div>
                        <img src={trashIcon} alt="trush " className="remove-item" onClick={() => {removeFromCart(item._id)}} />
                   </div>
-                </div>))}
-              </div>}
+                 </div>))}
+                 <div className="buy" onClick={() =>{Navigate('/checkout')}}>
+                  <h3 className="buy-text">Buy</h3>
+                  <img src={cartIcon} alt="cart Icon" /> 
+                 </div>
+               </div>
+              }              
           </>
      )
   }
