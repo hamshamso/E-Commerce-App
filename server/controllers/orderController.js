e8a2d7fba0fc68d40f78beb82866450716d71d29
hamshamso <hamchamco7@gmail.com>
2026-09-18 23:59:23 +0100

    img is fixed

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -172,7 +172,7 @@ const getDetailedOrders = async(req,res) =>{
       return res.status(404).json({ success: false, msg: "Order doesn't exist" });
     }
   //we can use this but it use too much RAM
-  //const filtredOrders = detailedorders.filter(order => order.items && order.items.length > 0)
+  //const filtredOrders = detailedorders.filter((order) => order.items && order.items.length > 0)
   return res.status(200).json( {success:true, data:detailedorders, msg:"Successfully get detailed orders" })
   }catch(err){
     return res.status(400).json({ success:false, msg:"Failed to get detailed order", message:err.message })
@@ -182,10 +182,10 @@ const getDetailedOrders = async(req,res) =>{
 const getOrderById = async(req,res) => {
   try{
     const id = req.params.id || req.params._id
-    const order = await Order.findById(id)
+    const order = await Order.findById(id).populate({path: 'items.product',select: ' image'});
     //Admin doesn't need an authorization
     if(order.items.length === 0){
-      return res.status(200).json( {success:true,empty:"true", msg:"This order is empty" })
+      return res.status(200).json( {success:true, empty:"true", msg:"This order is empty" })
     }
     if(order){
        return res.status(200).json( {success:true, data:order, msg:"Successfully get the order" })
