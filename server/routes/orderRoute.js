46184987b94f7b4774bc9af1c1a3be5f286f9f90
hamshamso <hamchamco7@gmail.com>
2026-09-19 21:53:22 +0100

    Add confirm and cancel order routes to order management

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import ValidateUser from '../middelwares/validatUser.js';
 import adminOnly from '../middelwares/adminOnly.js';
-import {createOrder,deleteOrder,getMyOrders,updateOrderStatus,getAllOrders,getProductsInfo,cancelProductFromOrder,getDetailedOrders,getOrderById} from '../controllers/orderController.js';
+import {createOrder,deleteOrder,getMyOrders,updateOrderStatus,getAllOrders,
+        getProductsInfo,cancelProductFromOrder,getDetailedOrders,getOrderById,
+        confirmOrder,canselOrder} from '../controllers/orderController.js';
 const Router = express.Router();
 
 Router.post('/orders', ValidateUser, createOrder)
@@ -10,6 +12,8 @@ Router.get('/orders/:id', ValidateUser, getProductsInfo)
 Router.get('/orders', ValidateUser, adminOnly, getAllOrders)
 Router.get('/dashboard/orders/:id',ValidateUser,adminOnly,getOrderById)
 Router.get('/dashboard/orders',ValidateUser,adminOnly,getDetailedOrders)
+Router.put('/dashboard/orders/:id/confirm',ValidateUser,adminOnly,confirmOrder)
+Router.put('/dashboard/orders/:id/cansel',ValidateUser,adminOnly,canselOrder)
 Router.put('/orders/:id/status', ValidateUser, adminOnly, updateOrderStatus)
 Router.delete('/orders/:id',ValidateUser,adminOnly,deleteOrder)
 Router.delete('/orders/:orderId/:productId', ValidateUser, cancelProductFromOrder)
